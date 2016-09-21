46f879ad976818ca326de1765f30bac1fa66ecba
davepcollins83 <davidcollins.409@gmail.com>
2016-09-21 17:23:29 +0100

    Update line_follow.c

--- line_follow.c
+++ line_follow.c
@@ -1,3 +1,34 @@
 // Code developed by RAF Leeming Hackspace
 // Line Following Robot for 90SU Cross Brief Hackathon
 
+// Define pin connections
+
+// LED
+int LED_RED = 9;      // PWM capable pin
+int LED_GREEN  = 10;  // PWM capable pin
+int LED_BLUE = 11;    // PWM capable pin
+
+// Motor 1
+int ENA = 6;          // PWM capable pin
+int N1 = 14;          // Pin 14 = A0, 15 = A1 etc
+int N2 = 15;
+
+// Motor 2
+int ENB = 9;          // PWM capable pin
+int N3 = 16;
+int N4 = 17;
+
+// Sensors
+int LEFT = 3;
+int MID = 4;
+int RIGHT = 5;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+}
