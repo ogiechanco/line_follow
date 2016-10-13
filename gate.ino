6a493d953d0397d201521333ec5e80a03b30eb5d
davepcollins83 <davidcollins.409@gmail.com>
2016-10-13 07:31:22 +0100

    Update gate.ino

--- gate.ino
+++ gate.ino
@@ -1,7 +1,11 @@
 // Laser Gate Code for 90SU Hackathon
 // (C) 2016 Dave Collins
 
+// constants
 
+
+
+// variables
 
 
 void setup(){
@@ -11,6 +15,17 @@ void setup(){
 
 
 void loop(){
-
+  
+  // detect rising/falling edge (beam broken)
+  
+  // increment lap
+  
+  // if lap = 0 - do nothing
+  
+  // if lap = n - increment lap n time by 1 tenth
+  
+  // if lap >= max - do nothing
+  
+  // update display - clear then show times 
 
 }
