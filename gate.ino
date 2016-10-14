ab86b9a52cc14b431b54c1bfc3eb534c5c448d9f
davepcollins83 <davidcollins.409@gmail.com>
2016-10-14 12:09:42 +0100

    Update gate.ino

--- gate.ino
+++ gate.ino
@@ -1,31 +1,96 @@
 // Laser Gate Code for 90SU Hackathon
 // (C) 2016 Dave Collins
 
-// constants
+int laser = 3;
+int sensor = 19;
+int lapCount = 0;
+int sensorOff = 0;
+int sensorOffTime = 0;
 
+int lastBool = 0;
 
-
-// variables
-
+int lapMin[4];
+int lapSec[4];
+int lapTen[4];
 
 void setup(){
-
-
+  
+ pinMode(laser, OUTPUT); 
+ pinMode(sensor, INPUT);
+ 
+ Serial.begin(9600);
+ 
+ digitalWrite(laser, HIGH);
+  
 }
-
 
 void loop(){
   
-  // detect rising/falling edge (beam broken)
+  int sensorValue = analogRead(sensor);
   
-  // increment lap
+  int sensorBool = 0;
   
-  // if lap = 0 - do nothing
+  timeCount++;
   
-  // if lap = n - increment lap n time by 1 tenth
+  if (sensorValue < 100){
+    
+    sensorBool = 0;
+    
+  }
+  else{
+    
+    sensorBool = 1;
+    
+  }
   
-  // if lap >= max - do nothing
+  if (sensorBool == 1 && sensorOff == 0){
+    if (lastBool == 0){
+    
+      // Rising Edge
+      
+      lapCount++;
+      
+      sensorOff = 1;
+      sensorOffTime = 0;
+      
+    }
+  }  
   
-  // update display - clear then show times 
-
+  sensorOffTime++;
+  
+  if(sensorOffTime > 50){
+    
+    sensorOff = 0;
+    
+  }
+  
+  
+  lapTen[lapCount]++;
+  
+  if(lapTen[lapCount] == 10){
+   
+    lapTen[lapCount] = 0;
+    lapSec[lapCount]++;
+    
+    if(lapSec[lapCount] == 60){
+      
+      lapSec[lapCount] == 0;
+      lapMin[lapCount]++;
+    }
+    
+  }
+  
+  
+  delay(100);
+  
+  Serial.println();
+  Serial.print(lapCount);
+  Serial.print("  ");
+  Serial.print(lapMin[lapCount]);
+  Serial.print(":");
+  Serial.print(lapSec[lapCount]);
+  Serial.print(".");
+  Serial.print(lapTen[lapCount]);
+  
+  lastBool = sensorBool;
 }
